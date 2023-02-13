caff959d43949e243d9d0e727c0a0877c226de1b
Kamarhinanton <Kamarhin_anton@ukr.net>
2023-02-13 10:12:35 +0200

    feat: add course

---
+++ src/store/action-creator/todo.ts
@@ -0,0 +1,26 @@
+import {Dispatch} from "redux";
+import axios from "axios";
+import {TodoAction, TodoActionTypes} from "../../types/todo";
+
+export const fetchTodos = (page = 1, limit = 10) => {
+  return async (dispatch: Dispatch<TodoAction>) => {
+    try {
+      dispatch({type: TodoActionTypes.FETCH_TODOS})
+      const response = await axios.get('https://jsonplaceholder.typicode.com/todos/',{
+        params: {_page: page, _limit: limit}
+      })
+      setTimeout(() => {
+        dispatch({type: TodoActionTypes.FETCH_TODOS_SUCCESS, payload: response.data})
+
+      }, 500)
+    } catch (e) {
+      dispatch({
+        type: TodoActionTypes.FETCH_TODOS_ERROR,
+        payload: 'Помилка при завантаженні списка справ'})
+    }
+  }
+}
+
+export function setTodoPage(page: number): TodoAction {
+  return {type: TodoActionTypes.SET_TODOS_PAGE, payload: page}
+}
